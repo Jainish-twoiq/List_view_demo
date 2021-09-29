88f23385811d7b035ea635137ce342b8123b9228
Jainish-twoiq <jainish.shah@twoiq.com>
2021-09-29 14:53:30 +0530

    Added card property and label property

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,41 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'JP',text: 'hakuna matata'),
     Quote(author: 'NJ', text: 'hello')
   ];
+  Widget quoteTemplate(quote){
+    return Card(
+      margin : EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child : Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+
+            ),
+            SizedBox(height: 8.0),
+            FlatButton.icon(
+              onPressed: () {},
+              label: Text('Delete quotes'),
+              icon: Icon(Icons.delete),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +66,8 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) {
-          return Text('${quote.text}-${quote.author}');
-        }).toList(),
+        children: quotes.map((quote) =>quoteTemplate(quote)).toList(),
+
       ),
     );
   }
